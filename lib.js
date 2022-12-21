784d024de827a26a04ea90a54b4916f9ba753482
Shital769 <110831382+Shital769@users.noreply.github.com>
2022-12-22 00:08:19 +1100

    learned testing  numbers and arrays

---
+++ lib.js
@@ -0,0 +1,11 @@
+//Testing Numbers
+
+module.exports.absoulte = function (number) {
+  return number >= 0 ? number : -number;
+};
+
+//Testing Strings
+
+module.exports.greet = function (name) {
+  return "Welcone " + name;
+};
--- tests/lib.test.js
+++ tests/lib.test.js
@@ -1,3 +1,27 @@
-test("Our first test", () => {
-    
+const lib = require("../lib");
+
+describe("absolute  ", () => {
+  it("should return a positive number if input is positive", () => {
+    const result = lib.absoulte(1);
+    expect(result).toBe(1);
+  });
+  it("should return a positive number if input is negative", () => {
+    const result = lib.absoulte(-1);
+    expect(result).toBe(1);
+  });
+  it("should return a 0 if input is 0", () => {
+    const result = lib.absoulte(0);
+    expect(result).toBe(0);
+  });
 });
+
+describe("greet", () => {
+  it("should return the greeting message", () => {
+    const result = lib.greet("Shital");
+    expect(result).toMatch(/Shital/);
+    expect(result).toContain("Shital");
+  });
+});
+
+
+//Testing Arrays
